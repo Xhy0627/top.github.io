16feed685b077bb5f20b49032926b3b804a7acfc
ileler <ileler@qq.com>
2018-05-16 15:00:08 +0800

    sync configs.ext

--- .IntelliJIdea/config/consoles/.history/db/d72bdeeb-d35b-45df-960b-6dca20ec27af.sql
+++ .IntelliJIdea/config/consoles/.history/db/d72bdeeb-d35b-45df-960b-6dca20ec27af.sql
@@ -5,3 +5,11 @@ select * from t_processing_file where goods_id = '119943';
 use mpr_pcs;
 ;-- -. . -..- - / . -. - .-. -.--
 ALTER TABLE t_consumer MODIFY birthday datetime DEFAULT '0000-00-00 00:00:00' COMMENT '出生日期';
+;-- -. . -..- - / . -. - .-. -.--
+use mpr_pgs;
+;-- -. . -..- - / . -. - .-. -.--
+select * from t_goods
+where id = 120069;
+;-- -. . -..- - / . -. - .-. -.--
+select * from t_publication_book
+where id = 119765;
--- .IntelliJIdea/config/consoles/db/d72bdeeb-d35b-45df-960b-6dca20ec27af/console.sql
+++ .IntelliJIdea/config/consoles/db/d72bdeeb-d35b-45df-960b-6dca20ec27af/console.sql
@@ -1,1 +1,9 @@
 ALTER TABLE t_consumer MODIFY birthday datetime DEFAULT '0000-00-00 00:00:00' COMMENT '出生日期';
+
+
+select * from t_goods
+where id = 120069
+
+
+select * from t_publication_book
+where id = 119765
