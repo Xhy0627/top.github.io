e83f03d5ce2538e4d7ef111fe0f709d919ac78b7
ileler <ileler@qq.com>
2018-05-18 08:35:26 +0800

    sync configs.ext

--- .IntelliJIdea/config/consoles/.history/db/d72bdeeb-d35b-45df-960b-6dca20ec27af.sql
+++ .IntelliJIdea/config/consoles/.history/db/d72bdeeb-d35b-45df-960b-6dca20ec27af.sql
@@ -2,8 +2,6 @@ USE mpr_cpps;
 ;-- -. . -..- - / . -. - .-. -.--
 select * from t_processing_file where goods_id = '119943';
 ;-- -. . -..- - / . -. - .-. -.--
-use mpr_pcs;
-;-- -. . -..- - / . -. - .-. -.--
 ALTER TABLE t_consumer MODIFY birthday datetime DEFAULT '0000-00-00 00:00:00' COMMENT '出生日期';
 ;-- -. . -..- - / . -. - .-. -.--
 use mpr_pgs;
@@ -13,3 +11,17 @@ where id = 120069;
 ;-- -. . -..- - / . -. - .-. -.--
 select * from t_publication_book
 where id = 119765;
+;-- -. . -..- - / . -. - .-. -.--
+use mpr_pcs;
+;-- -. . -..- - / . -. - .-. -.--
+select * from mpr_pcs.t_consumer where login_name like '13631693333';
+;-- -. . -..- - / . -. - .-. -.--
+select * from mpr_pcs.t_consumer where login_name not like '%@%';
+;-- -. . -..- - / . -. - .-. -.--
+update mpr_pcs.t_consumer set status = 0 login_name not like '%@%' and status = 1;
+;-- -. . -..- - / . -. - .-. -.--
+update mpr_pcs.t_consumer set status = 0 where login_name not like '%@%' and status = 1;
+;-- -. . -..- - / . -. - .-. -.--
+commit;
+;-- -. . -..- - / . -. - .-. -.--
+select * from mpr_pcs.t_consumer where login_name not like '%@%' and status = 1;
--- .IntelliJIdea/config/consoles/db/d72bdeeb-d35b-45df-960b-6dca20ec27af/console.sql
+++ .IntelliJIdea/config/consoles/db/d72bdeeb-d35b-45df-960b-6dca20ec27af/console.sql
@@ -1,9 +1,2 @@
-ALTER TABLE t_consumer MODIFY birthday datetime DEFAULT '0000-00-00 00:00:00' COMMENT '出生日期';
-
-
-select * from t_goods
-where id = 120069
-
-
-select * from t_publication_book
-where id = 119765
+#后台新增冻结，解冻后，登陆时判断了本地账号的状态。之前旧数据存在部分默认为未激活实际是激活了的数据，这里统一处理
+update mpr_pcs.t_consumer set status = 0 where login_name not like '%@%' and status = 1;
